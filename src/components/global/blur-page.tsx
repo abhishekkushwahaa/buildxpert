31ef38032b2a5d5cbeb7498a568493637bc577aa
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-07-11 15:25:55 +0530

    launchpad

--- src/components/global/blur-page.tsx
+++ src/components/global/blur-page.tsx
@@ -8,7 +8,7 @@ const BlurPage = ({ children }: Props) => {
   return (
     <ScrollArea className="h-screen w-full rounded-md border">
       <div
-        className="min-h-full backdrop-blur-[35px] dark:bg-muted/40 bg-muted/40 dark:shadow-2xl dark:shadow-black mx-auto pt-20 p-4 md:ml-[50px] lg:ml-[50px] top-0 right-0 left-0 bottom-0 z-[11]"
+        className="min-h-full backdrop-blur-[35px] dark:shadow-2xl dark:shadow-black mx-auto pt-20 p-4 md:ml-[50px] lg:ml-[50px] top-0 right-0 left-0 bottom-0 z-[11]"
         id="blur-page"
       >
         {children}
